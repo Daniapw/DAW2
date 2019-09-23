9df391efb7d626a01ac4844c653d5ca756cd4050
Daniapw <daniruiz51@gmail.com>
2019-09-23 20:09:35 +0200

    Commit 4
    Ejercicio 3 resuelto

--- DWEC/ejerciciosJS/ejercicio3.js
+++ DWEC/ejerciciosJS/ejercicio3.js
@@ -1,12 +1,37 @@
 function comprobarTriangulo(l1, l2, l3, a1, a2, a3){
 
-    sumaAngulos=a1+a2+a3;
+    sumaAngulos=(a1+a2+a3);
 
+    if(!l1 || !l2 || !l3){
+        alert("No es un triángulo, faltan lados")
+    }
+    else{
 
-    if((l1 == l2 && l1==l3) && sumaAngulos==180)
-        alert("Es un triángulo equilátero");
-    
+        if ((l1 == l2 && l1==l3) && sumaAngulos==180){
+            alert("Es un triángulo equilátero");
+        }
+        else if ((l1==l2 || l1==l3 || l2==l3) && (a1==a2 || a1==a3 || a2==a3)){
+            alert("Es un triángulo isósceles");
+        }
+        else if ((l1!=l2 && l1!=l2 && l2!=l3) && (a1!=a2 && a1!=a3 && a2!=a3)){
+            alert("Es un triángulo escaleno");
+        }
 
-    else ((l1==l2 || l1==l3 || l2==l3) && (a1==a2 || a1==a3 || a2==a3))
-        alert("Es un triángulo isósceles")
+        if (isRectangulo(a1,a2,a3))
+            alert("Es rectángulo")
+        else
+            alert("No es rectángulo")
+    }
+}
+
+//Comprobar si el triángulo es rectángulo
+function isRectangulo(a1, a2, a3){
+
+    if((a1==90 && a2<90 && a3<90) || (a2==90 && a1<90 && a3<90) ||
+        (a3==90 && a1<90 && a2<90)){
+
+        return true;
+    }
+
+    return false;
 }
