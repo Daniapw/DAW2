86694ddae7aa189b0cd7cf8762a1048b729c47b6
Daniapw <daniruiz51@gmail.com>
2019-10-08 10:26:55 +0200

    AAA

--- DWES/ejerciciosSueltos/confirmar.php
+++ DWES/ejerciciosSueltos/confirmar.php
@@ -7,14 +7,9 @@ Curso: <?php echo($_POST["curso"]); ?><br>
 
 <form action="formularioBasico2.php" method="post">
     
-    <input type="hidden" name="numMatricula" value="" /><br>
-    <input type="hidden" name="curso" value="" />
     <input type="hidden" name="nombre" value="<?php echo($_POST["nombre"])?>"/>
     <input type="hidden" name="apellidos" value="<?php echo($_POST["apellidos"])?>"/>
-    <input type="submit" value="Canclear" name="cancelar" />
-    
-</form>
-
-<form action="formularioBasico2.php" method="post">
+    <input type="submit" value="Cancelar" name="cancelar" />
     <input type="submit" value="Aceptar" name="aceptar"/>
+    
 </form>
--- DWES/ejerciciosSueltos/formularioBasico2.php
+++ DWES/ejerciciosSueltos/formularioBasico2.php
@@ -1,12 +1,12 @@
 <?php
 require './formularioUtils.php';
 
-if (isSet($_POST["sig"]) && formularioRelleno(3)){
+if (isSet($_POST["sig"])){
 ?>
 <form action="confirmar.php" method="post">
     <h1>Datos matrícula</h1>
-    Nº matrícula:<input type="text" name="numMatricula" value="<?php rellenarInputTexto("nombre", "aceptar");?>" required/><br>
-    Curso: <input type="text" name="curso" value="<?php rellenarInputTexto("nombre", "aceptar");?>" required/>
+    Nº matrícula:<input type="text" name="numMatricula" value="" required/><br>
+    Curso: <input type="text" name="curso" value="" required/><br>
     <input type="hidden" name="nombre" value="<?php echo($_POST["nombre"])?>"/>
     <input type="hidden" name="apellidos" value="<?php echo($_POST["apellidos"])?>"/>
     <input type="submit" value="Siguiente" name="sig2" />
@@ -14,21 +14,23 @@ if (isSet($_POST["sig"]) && formularioRelleno(3)){
 <?php
 }
 else{
-    if(isset($_POST["sig"]) && !formularioRelleno(3)){
+    if(isset($_POST["sig"])){
         echo('<script language="javascript">alert("Debe rellenar todos los datos del formulario")</script>');
     }
 ?>
     <form action="formularioBasico2.php" method="post">
 
         <h1>Datos personales</h1>
-        Nombre: <input type="text" name="nombre" value="<?php if(isset($_POST["cancelar"])){ 
-                                                                rellenarInputTexto("nombre", "cancelar");
-                                                            }
-                                                            else{
-                                                                rellenarInputTexto("nombre", "sig");
-                                                            }
-?>" /><br/>
-        Apellido:<input type="text" name="apellidos" value="<?php rellenarInputTexto("apellidos", "aceptar");?>" /><br>
+        Nombre: <input type="text" name="nombre" required value="<?php rellenarInputTexto("nombre", "cancelar"); ?>" /><br/>
+        Apellido:<input type="text" name="apellidos" required value="<?php rellenarInputTexto("apellidos", "cancelar"); ?>" /><br>
+        <?php
+            //Guardar datos matricula y curso si el usuario le da a cancelar
+            if (isset($_POST["cancelar"])){?>
+
+                <input type="hidden" name="numMatricula" value="<?php echo($_POST["numMatricula"])?>"/>
+                <input type="hidden" name="curso" value="<?php echo($_POST["curso"])?>"/>
+      <?php } ?>
+        
         <input type="submit" value="Siguiente" name="sig"/>
     </form>
 <?php
