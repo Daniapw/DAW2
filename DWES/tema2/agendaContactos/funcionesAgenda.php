381c7136d8a68376a8298224fe1601925a4c1926
Daniapw <daniruiz51@gmail.com>
2019-10-12 17:22:03 +0200

    DWES
    Agenda para revision

--- DWES/tema2/agendaContactos/funcionesAgenda.php
+++ DWES/tema2/agendaContactos/funcionesAgenda.php
@@ -1,13 +1,15 @@
 <?php
-
+//Mostrar agenda
 function mostrarAgenda($agenda){
     echo
-    "<table border='1'>
+    "<h1>Contactos</h1><br>
+    <table border='1'>
         <tr>
             <th>Nombre</th>
             <th>Telefono</th>
         </tr>";
     
+    //Imprimir contactos
     foreach ($agenda as $nombre => $telefono) {
         
         if ($nombre!="enviar"){
@@ -21,4 +23,58 @@ function mostrarAgenda($agenda){
     
     echo("</table>");
 
+}
+
+//Funcion para agenda
+function agenda($agenda){
+    
+    $tipoOperacion="";
+    
+    //Si el telefono no esta vacio
+    if(!empty($_POST["telefono"]) && !empty($_POST["nombre"])){
+        if (array_key_exists($_POST["nombre"], $agenda)){
+            $tipoOperacion="modificar";
+        }
+        //Si no lo esta
+        else{
+            $tipoOperacion="anadir";
+        }
+        
+        $agenda[$_POST["nombre"]]=$_POST["telefono"]; 
+    }
+    //Si el nombre esta en la agenda y el telefono esta vacio
+    elseif(array_key_exists($_POST["nombre"], $agenda) && empty($_POST["telefono"])){
+        unset($agenda[$_POST["nombre"]]);
+        $tipoOperacion="eliminar";
+    }
+    
+    echo mensaje($agenda, $tipoOperacion);
+    return $agenda;
+}
+
+//Funcion para determinar el mensaje que va a salir
+function mensaje($agenda, $tipoOperacion){
+    $mensaje="<p style='font-size:30px; color:";
+    
+    //Segun el tipo de operacion el mensaje cambia de contenido y color
+    switch ($tipoOperacion){
+        case "eliminar":{
+            $mensaje=$mensaje."blue;'>Contacto eliminado con exito";
+            break;
+        }
+        case "modificar":{
+            $mensaje=$mensaje."darkgreen;'>Contacto ".$_POST["nombre"]." modificado con exito";
+            break;
+        }case "anadir":{
+            $mensaje=$mensaje."darkgreen;'>Contacto ".$_POST["nombre"]." añadido con exito";
+            break;
+        }
+        case "":{
+            $mensaje=$mensaje."red;'>Error: El contacto debe tener un nombre";
+            break;
+        }
+        
+    }
+    
+    return $mensaje;
 }
--- DWES/tema2/agendaContactos/indexAgenda.php
+++ DWES/tema2/agendaContactos/indexAgenda.php
@@ -1,43 +1,80 @@
 <html>
 <head>
+    <style>
+        body{
+            text-align:center;
+        }
+        
+        #formulario{
+            border: solid 1px;
+            position:fixed;
+            padding:10px;
+            right:40px;
+            font-size:20px;
+            
+        }
+        
+        #agenda{
+            margin:auto;
+            width:50%;
+            text-align:center;
+
+        }
+        
+        #agenda table{
+            text-align:center;
+            margin:auto;
+            width:50%;
+            font-size:20px;
+        }
+       
+        
+    </style>
 </head>
 <body>
 <?php
 require 'funcionesAgenda.php';
 
 if (isSet($_POST["enviar"])){
-    $contador=1;
-    $agenda=array();
     
-    var_dump($_POST);
-    foreach ($_POST as $valor) {
-        
-        if($valor!="Enviar"){
-            
-            switch($contador){
-                case 1:{
-                    $nombre=$valor;
-                    $contador++;
-                    break;
-                }
-                case 2:{
-                    $agenda[$nombre]=$valor;
-                    $contador=1;
-                    break;
-                }  
-            }
-            
-        }
+    //Si ya se han anadido contactos a la agenda
+    if(isSet($_POST["agenda"])){    
+        $agendaCodificada=$_POST["agenda"];
+        $agenda=json_decode($agendaCodificada, true);    
+    }
+    else{
+        $agenda=[];
     }
     
-    echo(mostrarAgenda($agenda));
+    $agenda= agenda($agenda); 
+    
+    ?>
+    
+    <div id="agenda">
+        <?php  echo(mostrarAgenda($agenda)); ?>
+    </div>
+    
+    <div id="formulario">
+        <h1>Añadir contacto</h1>
+        <form action="indexAgenda.php" method="post">
+            Nombre: <input type="text" name="nombre" value="" /><br>
+            Telefono: <input type="text" name="telefono" value=""/><br>
+            <input type="hidden" name="agenda" value='<?php echo json_encode($agenda); ?>'/>
+            <input type="submit" value="Enviar" name="enviar" />
+        </form>
+
+    </div>
+    
+    <?php 
 }
 else{?>
+    
     <form action="indexAgenda.php" method="post">
         Nombre: <input type="text" name="nombre" value="" required /><br>
         Telefono: <input type="text" name="telefono" value="" required/><br>
         <input type="submit" value="Enviar" name="enviar" />
     </form>
+    
 <?php
 }
 ?>
